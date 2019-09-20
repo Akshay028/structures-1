ad552a66656b38e0349c054f1496c62ab41bd4a7
Robbie VanDerzee <Robert.Scott.VanDerzee@Gmail.com>
2019-09-20 18:44:40 -0400

    Added constructor(size). Added operators. Added size()/capacity() retrieval. Tests increased. __DEFAULT_CAPACITY modified to = e^3. Standardized constructors.

--- stack/stack.hpp
+++ stack/stack.hpp
@@ -4,7 +4,7 @@
 #include <cstring>
 #include <stdexcept>
 
-#define __DEFAULT_CAPACITY 32
+#define __DEFAULT_CAPACITY 20
 #define __INITIAL_SIZE 0
 
 double const e = 2.7182818284590452353602874713527;
@@ -13,10 +13,30 @@ namespace structures {
 template <typename T>
 class stack {
  public:
-  stack() : _capacity(__DEFAULT_CAPACITY), _size(__INITIAL_SIZE) {
+  stack(){
+    _capacity = __DEFAULT_CAPACITY;
+    _size = __INITIAL_SIZE;
     _stack = new T[_capacity];
   }
+  stack(int __init_capacity) {
+    _size = __INITIAL_SIZE;
+    _capacity = __init_capacity;
+    _stack = new T[_capacity];
+  }
+  stack& operator=(const stack& source) {
+    if (this == &source)
+      return *this;
+    else {
+      this->empty();
+      this->resize(source._capacity);
+      this->_size = source._size;
+      std::memcpy(this->_stack, source._stack, _size * sizeof(source._stack));
+    }
+    return *this;
+  }
   ~stack() { delete[] _stack; }
+  int size() const { return _size; }
+  int capacity() const { return _capacity; }
   void pop() {
     if (is_empty())
       throw std::range_error("Attempted to pop an empty stack.\n");
@@ -26,12 +46,13 @@ class stack {
     if (_size == _capacity) grow();
     _stack[_size++] = element;
   }
-  T top() {
+  T top() const {
     return (!is_empty())
                ? _stack[_size - 1]
                : throw std::range_error("Attempted to top an empty stack.\n");
   }
-  bool is_empty() { return _size <= __INITIAL_SIZE; }
+  bool is_empty() const { return _size <= __INITIAL_SIZE; }
+  void empty() { _size = 0; }
   void grow() {
     int _capacity_old = _capacity;
     _capacity *= e;
@@ -39,6 +60,11 @@ class stack {
     std::memcpy(_stack2, _stack, _capacity_old * sizeof(*_stack));
     delete[] _stack;
     _stack = _stack2;
+  }
+  void resize(int __capacity_new) {
+    delete[] _stack;
+    _capacity = __capacity_new;
+    _stack = new T[_capacity];
   }
 
  private:
--- stack/stack_test_harness.cpp
+++ stack/stack_test_harness.cpp
@@ -2,12 +2,42 @@
 #include "stack.hpp"
 int main() {
   structures::stack<double> s;
+  structures::stack<double> s2(200);
+  std::cout << s2.capacity() << '\n';
   for (int i = 0; i < 100; i++) {
     s.push(i * 1.0);
+    s2.push(i * 1.0);
   }
   while (!s.is_empty()) {
-    std::cout << s.top() << '\n';
+    std::cout << s.top() << '\t';
     s.pop();
+  }
+  std::cout << "end of stack" << '\n';
+  for (int i = 0; i < 32; i++) {
+    s.push(i * 1.0);
+  }
+  s.empty();
+  s.push(30);
+  s.top();
+  s.pop();
+  if (s.is_empty()) {
+    std::cout << "s is empty" << '\n';
+  }
+  s = s2;
+  std::cout << "Checking = operator" << '\n';
+  while (!s.is_empty()) {
+    std::cout << s.top() << '\t';
+    s.pop();
+  }
+  if (s.is_empty()) {
+    std::cout << "s is empty" << '\n';
+  } else {
+    std::cout << "s is not empty" << '\n';
+  }
+  if (s2.is_empty()) {
+    std::cout << "s2 is empty" << '\n';
+  } else {
+    std::cout << "s2 is not empty" << '\n';
   }
   return 0;
 }
