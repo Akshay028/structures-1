0c7c842e18a68b85de1e3d9058474fc8c42973d3
Robbie VanDerzee <Robert.Scott.VanDerzee@Gmail.com>
2019-10-08 08:59:45 -0400

    Transitioning to nested class model.

--- graphs/binary_tree.hpp
+++ graphs/binary_tree.hpp
@@ -1,11 +1,29 @@
 #ifndef BINARY_TREE_HPP
 #define BINARY_TREE_HPP
 
+#include "node.hpp"
+
 namespace structures {
+  template <typename T>
   class binary_tree {
   public:
-  private:
+    binary_tree(){
 
+    }
+    ~binary_tree(){
+
+    }
+    void insert(T data){}
+    Node<T>& find(T data){
+      return NULL;
+    }
+    bool remove(T data){
+      if(find(T data) == NULL) return true;
+      return false;
+    }
+  private:
+    Node<T> *_root;
+    Node<T> *_l, *_r;
   };
 } /* structures */
 
